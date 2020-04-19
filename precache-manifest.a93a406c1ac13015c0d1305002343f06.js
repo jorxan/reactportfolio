bade6d4af84026905b90533cb446d13c06b6703e
Jordan Albers <jordanalbers@Jordans-MacBook-Air.local>
2020-04-18 21:33:26 -0700

    Updates

---
+++ precache-manifest.a93a406c1ac13015c0d1305002343f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9582ecdfa6cfb4afdc1b5b917948dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a367b3d7ea9848b2394",
+    "url": "/static/css/main.e7b5f1b3.chunk.css"
+  },
+  {
+    "revision": "491ff2e110d07559bf46",
+    "url": "/static/js/2.f2e85f79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2e85f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a367b3d7ea9848b2394",
+    "url": "/static/js/main.b219d4a4.chunk.js"
+  },
+  {
+    "revision": "586fdfc8a8030a7117d4",
+    "url": "/static/js/runtime-main.0eeac3f4.js"
+  }
+]);
